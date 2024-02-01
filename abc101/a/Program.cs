616d16ef206d9aa79c40c6e965ac2cf651010d47
SotaHiguchi <sota.higuchi@student.torrens.edu.au>
2024-02-01 23:37:55 +1100

    add environment setup

---
+++ abc101/a/Program.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace x
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            // Console.SetOut(sw);
+
+            // // 文字列の入力
+            // string s = Console.ReadLine();
+
+            // // 整数の入力
+            // long n = long.Parse(Console.ReadLine());
+
+            // // 文字列配列の入力
+            // string[] inputStrArray = Console.ReadLine().Split(' ');
+
+            // // 整数配列の入力
+            // var inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+
+            string input = Console.ReadLine();
+
+            int count = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                var temp = input.Substring(i, 1);
+                if (temp == "+") count++;
+                if (temp == "-") count--;
+            }
+
+            string result = count.ToString();
+            Console.WriteLine(result);
+        }
+
+        // Console.WriteLine(int.Parse(result));
+
+        // Console.Out.Flush();
+
+    }
+
+    //よく使うコードなどあれば    
+    class Utils
+    {
+    }
+}
