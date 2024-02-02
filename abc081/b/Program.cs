064abd442d2afb29f591f77ea580219d01ab6b62
SotaHiguchi <sota.higuchi@student.torrens.edu.au>
2024-02-02 12:09:47 +1100

    add 081b

--- abc081/b/Program.cs
+++ abc081/b/Program.cs
@@ -8,25 +8,31 @@ namespace x
     {
         static void Main(string[] args)
         {
-            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
-            Console.SetOut(sw);
-
             // 文字列の入力
             string s = Console.ReadLine();
-
-            // 整数の入力
-            long n = long.Parse(Console.ReadLine());
 
             // 文字列配列の入力
             string[] inputStrArray = Console.ReadLine().Split(' ');
 
-            // 整数配列の入力
-            var inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+
+            int minCnt = 99999;
+            for (int i = 0; i < int.Parse(s); i++)
+            {
+                int shou = int.Parse(inputStrArray[i]);
+                int cnt = 0;
+                while (shou % 2 == 0)
+                {
+                    shou = shou / 2;
+                    cnt++;
+                }
+                if (cnt < minCnt)
+                {
+                    minCnt = cnt;
+                }
+            }
 
 
-
-
-            string result = "";
+            int result = minCnt;
 
             Console.WriteLine(result);
 
