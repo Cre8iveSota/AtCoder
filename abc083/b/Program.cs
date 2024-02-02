6e7ab23e47089a567756ec6d02d1d4c22ff690cd
SotaHiguchi <sota.higuchi@student.torrens.edu.au>
2024-02-02 18:24:01 +1100

    add abc083b

--- abc083/b/Program.cs
+++ abc083/b/Program.cs
@@ -8,29 +8,45 @@ namespace x
     {
         static void Main(string[] args)
         {
-            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
-            Console.SetOut(sw);
-
-            // 文字列の入力
-            string s = Console.ReadLine();
-
-            // 整数の入力
-            long n = long.Parse(Console.ReadLine());
-
-            // 文字列配列の入力
-            string[] inputStrArray = Console.ReadLine().Split(' ');
-
             // 整数配列の入力
             var inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
 
+            long N = inputLongArray[0];
+            long A = inputLongArray[1];
+            long B = inputLongArray[2];
 
 
-
-            string result = "";
+            string result = Caliculation(N, A, B).ToString();
 
             Console.WriteLine(result);
 
             Console.Out.Flush();
+        }
+
+
+        static long Caliculation(long N, long A, long B)
+        {
+            long sum = 0;
+            for (long i = 0; i <= N; i++)
+            {
+                if (CalculationDigit(i) >= A && CalculationDigit(i) <= B)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+
+        static long CalculationDigit(long num)
+        {
+            long sum = 0;
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            return sum;
         }
     }
 
