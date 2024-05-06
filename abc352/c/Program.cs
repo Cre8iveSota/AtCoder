5d43757b28d392fa9a98f92e918b69a26d1868b6
SotaHiguchi <sota.higuchi@student.torrens.edu.au>
2024-05-07 02:42:32 +1000

    add atcoder

---
+++ abc352/c/Program.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace x
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region debug
+            if (false)
+            {
+                var exStdIn = new System.IO.StreamReader("./test/sample-3.in");
+                System.Console.SetIn(exStdIn);
+            }
+            #endregion
+
+            int N = int.Parse(Console.ReadLine());
+            int[] A = new int[N];
+            int[] B = new int[N];
+            int targetIndex = 0;
+            int maxDiff = 0;
+            long ans = 0;
+            for (int i = 0; i < N; i++)
+            {
+                string[] line = Console.ReadLine().Split(' ');
+                A[i] = int.Parse(line[0]);
+                B[i] = int.Parse(line[1]);
+                if (maxDiff < B[i] - A[i])
+                {
+                    maxDiff = B[i] - A[i];
+                    targetIndex = i;
+                }
+                ans += A[i];
+            }
+            ans -= A[targetIndex];
+            ans += +B[targetIndex];
+            Console.WriteLine(ans);
+
+        }
+    }
+
+    //よく使うコードなどあれば    
+    class Utils
+    {
+        // 文字列の入力
+        public static string StringIn() { return Console.ReadLine(); }
+        // 文字配列
+        public static string[] StringArrayIn() { return Console.ReadLine().Split(' '); }
+        // 単数整数配列の入力
+        public static int[] IntArray1In()
+        {
+            int[] line = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+            return line;
+        }
+        public static (int N1, int N2) IntArray2In()
+        {
+            int[] line = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+            return (line[0], line[1]);
+        }
+        // 複数整数配列の入力を各indexに基づき、index毎にそれぞれの変数に保存
+        public static (int[] expectedInputData1, int[] expectedInputData2) IntArrayDoubleIn(int maxNum)
+        {
+            int[] expectedInputData1 = new int[maxNum];
+            int[] expectedInputData2 = new int[maxNum];
+            for (int i = 0; i < maxNum; i++)
+            {
+                string[] line = Console.ReadLine().Split(' ');
+                expectedInputData1[i] = int.Parse(line[0]);
+                expectedInputData2[i] = int.Parse(line[1]);
+            }
+            return (expectedInputData1, expectedInputData2);
+        }
+
+        // 座標複数
+        public static (int x, int y, int z)[] IntXyzIn(int maxNum)
+        {
+            (int x, int y, int z)[] xyz = new (int, int, int)[maxNum];
+            for (int i = 0; i < maxNum; i++)
+            {
+                string[] line = Console.ReadLine().Split(' ');
+                xyz[i].x = int.Parse(line[0]);
+                xyz[i].y = int.Parse(line[1]);
+                xyz[i].z = int.Parse(line[2]);
+            }
+            return xyz;
+        }
+
+        // ジャグ配列
+        public static int[][] IntJaggedArrayIn(int maxNum)
+        {
+            int[][] jaggedArray = new int[maxNum][];
+            for (int i = 0; i < maxNum; i++)
+            {
+                jaggedArray[i] = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+            }
+            return jaggedArray;
+        }
+
+        //bit
+        public static int[] DecimalToBit(int decimalNum, int intendedDigit)
+        {
+            int[] bit = new int[intendedDigit];
+            for (int i = 0; i < intendedDigit; i++)
+            {
+                int div = 1 << i;
+                bit[i] = (decimalNum / div) % 2;
+            }
+            return bit;
+        }
+    }
+}
